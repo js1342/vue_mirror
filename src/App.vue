226e7d264335dcc11d0be646c42a64ca4a8b4fef
SHk <shbilly00@gmail.com>
2020-12-11 09:56:26 +0900

    test

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,7 @@ export default {
   },
   data(){
     return {
-      page : 2
+      page : 0
     }
   }
 }
--- src/components/IndexHeader.vue
+++ src/components/IndexHeader.vue
@@ -1,12 +1,22 @@
 <template>
   <div class="top-header">
+    <div v-if="authState !== 'signedin'">You are signed out.
+    <amplify-authenticator>
+        <amplify-sign-in header-text="Sign In Page"></amplify-sign-in>
+
+        <amplify-sign-out></amplify-sign-out>
+    </amplify-authenticator>
+    </div>
+    <div v-if="authState === 'signedin' && user">
+      <div>Hello, {{user.username}}</div>
+    </div>
     <weather/>
   </div>
 </template>
 
 <script>
 import Weather from './Weather.vue'
-
+import { onAuthUIStateChange } from '@aws-amplify/ui-components'
 export default {
   name:'IndexHeader',
   components:{
